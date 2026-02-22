363bef7388ec5c126ab0b941901b86ba3e58bc50
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-02-22 23:53:31 +0000

    added common.mjs to index file to debug in the consol

--- script.mjs
+++ script.mjs
@@ -4,8 +4,8 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import getDictionarySize from "./common.mjs";
+// import {getDictionarySize} from "./common.mjs";
 
-window.onload = function() {
-    document.querySelector("body").innerText = `There are ${getDictionarySize()} words in the Basic English dictionary`;
-}
+// window.onload = function() {
+//     document.querySelector("body").innerText = `There are ${getDictionarySize()} words in the Basic English dictionary`;
+// }
