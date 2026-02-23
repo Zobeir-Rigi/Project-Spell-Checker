dd552c73f7d3c96f524e4e384411eaefd4531b6f
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-02-23 00:54:56 +0000

    this function spliting the word by any space, hyphen, punctuation, and then push it to words

--- common.mjs
+++ common.mjs
@@ -1,3 +1,24 @@
 import words from "./words.json" with { type: "json" };
 
-export const getDictionarySize = () => words.length;
+export const dictionary = new Set(words)
+export const getDictionarySize = () => dictionary.size;
+
+export function splitTextToWords(text) {
+  const words = [];
+  const rawWords = text.split(/\s+/);
+
+  for (const rawWord of rawWords) {
+    const hyphenParts = rawWord.split("-");
+
+    for (const part of hyphenParts) {
+      const cleanWord = part.replace(/^[^\w]+|[^\w]+$/g, "");
+
+      if (cleanWord) {
+        words.push(cleanWord);
+      }
+    }
+  }
+
+  return words;
+}
+console.log(splitTextToWords("red- orange fir.  e,./, blue??-green!"))
